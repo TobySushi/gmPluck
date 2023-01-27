74b8c0a947a00f46ea4e75793e7f0bb325ec77ef
Toby Kind <sushidev2@gmail.com>
2023-01-27 11:32:44 -0500

    Initial commit
    Just basic user interface stuff right now

---
+++ Core/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace gmPluck.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+
+        // this is the event that runs when a property changes
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
