82dda552ad0e1444a427e46d5b68fe1b03884634
Toby Kind <sushidev2@gmail.com>
2023-01-31 10:16:50 -0500

    Worked on treeview styling
    Font color, font size, etc.

--- obj/Debug/net6.0-windows/App.g.cs
+++ obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace gmPluck {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace gmPluck {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public static void Main() {
             gmPluck.App app = new gmPluck.App();
             app.InitializeComponent();
